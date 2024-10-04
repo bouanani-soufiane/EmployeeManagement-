d74ea5c212c21dade6fc04341e630c522f4de827
bouanani-soufiane <soufian.bouanani2013@gmail.com>
2024-10-04 17:11:57 +0100

    added : second level caching with ehcache

--- src/main/java/ma/yc/Main.java
+++ src/main/java/ma/yc/Main.java
@@ -1,5 +1,7 @@
 package ma.yc;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import ma.yc.entity.Employee;
 import ma.yc.service.EmployeeService;
 import ma.yc.service.EmployeeServiceImpl;
@@ -15,5 +17,30 @@ public class Main {
         for (Employee employee : employees) {
             System.out.println(employee);
         }
+
+        Employee newEmployee = new Employee();
+        newEmployee.setDepartment("HR");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            String jsonText = objectMapper.writeValueAsString(newEmployee);
+            System.out.println("Serialized JSON: " + jsonText);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Employee employee = new Employee("John Doe", "123" ,"dd","ee","ss");
+            String json = serializeToJson(employee);
+            System.out.println("Serialized JSON: " + json);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String serializeToJson(Employee employee) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(employee);
     }
 }
